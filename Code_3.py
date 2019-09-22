19ed3f2ab95f5f4e2bc9b0562f2ef7a5f17253b5
Nitesh Kumar <nitesh.kumar@tamu.edu>
2019-09-22 12:54:32 -0500

    Introduction about important functionality in python for manipulating data.

---
+++ Code_3.py
@@ -0,0 +1,9 @@
+# program to manipulate a list, arrays, matrix using numpy and inbuilt python functions
+# Some handy inbuilt functions for list manipulation are:
+# list.append()
+# list.pop()
+# list.insert()
+# list.index()
+# Some crucial numpy operations are:
+# np.square(), np.sum(), np.exp(), np.sum(), np.ones(), np.zeros(), np.random.random() and much more
+
